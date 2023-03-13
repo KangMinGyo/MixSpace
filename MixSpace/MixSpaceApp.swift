56d99fdb6a514ab199a3dd0da730e94f9ef767cf
KangMinGyo <kang15567@kakao.com>
2023-03-13 15:28:17 +0900

    First Commit

---
+++ MixSpace/MixSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixSpaceApp.swift
+//  MixSpace
+//
+//  Created by KangMingyo on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct MixSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
